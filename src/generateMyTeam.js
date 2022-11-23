1f0cb9a90280fbafadfdb6c03ce003f97c039765
saitek <saitek300@gmail.com>
2022-11-22 21:02:43 -0800

    added push functions to manager and intern

--- src/generateMyTeam.js
+++ src/generateMyTeam.js
@@ -46,7 +46,11 @@ function generateMyTeam(team) {
       </div>`
     }
     const tempArr = []
+    tempArr.push(team.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager)).join(''))
+
     tempArr.push(team.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)).join(''))
+
+    tempArr.push(team.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)).join(''))
     return tempArr.join('')
 
 }
